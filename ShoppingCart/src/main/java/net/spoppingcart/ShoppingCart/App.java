e79d23dc2c2ecfe91a2e09be4988dfea34f98b76
pankaj <pankaj@gmail.com>
2018-01-09 12:54:45 +0530

    My project setup is completed successfully

---
+++ ShoppingCart/src/main/java/net/spoppingcart/ShoppingCart/App.java
@@ -0,0 +1,13 @@
+package net.spoppingcart.ShoppingCart;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
